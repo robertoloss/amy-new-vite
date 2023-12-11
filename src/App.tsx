bb2f8f0f7bb76a15ff8f6049d60b440e863190f6
Roberto Loss <robertoloss@gmail.com>
2023-12-10 23:28:55 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+import { client } from '../sanity/lib/client.ts'
+
+function App() {
+	const [projects, setProjects] = useState<{title: string}[]| null>(null)
+
+	
+	useEffect(()=>{
+		async function getProjects() {
+			const data = await client.fetch(
+				`*[_type == "project"]`
+			)
+			setProjects(data);
+			console.log(data);
+		}
+		getProjects();
+		
+	},[setProjects])	
+	
+	if (projects) {
+		console.log("projects : ", projects)
+	}
+
+  return (
+    <>
+      <div>	Hello there! </div>
+			{projects && projects.map(( project, index: number ) => 
+				<h1 key={index}>{project.title}</h1>
+			)}
+    </>
+  )
+}
+
+export default App
